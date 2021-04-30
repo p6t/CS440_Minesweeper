3976e47b62d25e22f10fb1c35e1f4b2a6bcd67e4
Mayank Singamreddy <singamreddy.mayank@gmail.com>
2021-04-30 10:16:32 -0400

    minor changes

--- advanced_agent.py
+++ advanced_agent.py
@@ -21,7 +21,6 @@ class AdvancedAgent:
 
     # Returns (x,y) for next cell to query
     def query_next(self):
-
         if self.firstmove == True:
             self.firstmove = False
             return ((1,1))
@@ -33,12 +32,14 @@ class AdvancedAgent:
         print("bluesclues in query")
         print(self.bluesclues)
         bluescluesItems = self.bluesclues.values()
+        
         print("blues clues items")
         print(bluescluesItems)
         for everyVar in bluescluesItems:
             for everyCoord in everyVar[1]:
                 if everyCoord not in allVarsList:
                     allVarsList.append(everyCoord)
+
         print("all vars list")
         print(allVarsList)
         # all vars is a list of all hidden, to allow for easier access
@@ -71,13 +72,24 @@ class AdvancedAgent:
         while indexCounter<len(answer):
             if answer[indexCounter] == False:
                 return allVarsList[indexCounter]
+            else:
+                target = random.randrange(0, self.n_hidden)
+                counter = 0
+                for i in range(self.d):
+                    for j in range(self.d):
+                        if (self.revealed[i][j] == 0):
+                            if (counter == target):
+                                return (i, j)
+                            else:
+                                counter += 1
 
     def update_kb(self, x, y, clue):
         print("clue")
         print(clue)
         if (clue == "mine"):
             # Hit a mine
-
+            self.is_mine[x][y] = 1
+            self.revealed[x][y] = 1
             for i in range(x - 1, x + 2):
                 if (i < 0) or (i >= self.d):
                     continue
@@ -87,16 +99,17 @@ class AdvancedAgent:
                     if (i == x) and (j == y):
                         continue
 
-                    if(((i,j)) in bluesclues[((i,j))][1]):
-                        if len(self.bluesclues[((i,j))][1]) == 0:
-                            del self.bluesclues[((i,j))]
-                        self.bluesclues[((i,j))][0]-=1
+                    if (i,j) in self.bluesclues and self.revealed[i][j] == 1:
+                        if((i,j) in self.bluesclues[(i,j)][1]):
+                            if len(self.bluesclues[((i,j))][1]) == 0:
+                                del self.bluesclues[((i,j))]
+                            self.bluesclues[((i,j))][0]-=1
 
         else:
             self.is_mine[x][y] = 0
             self.revealed[x][y] = 1
             answerList = self.count_adjacent(x,y)
-            self.bluesclues[((x,y))] = [clue,answerList[1]]
+            self.bluesclues[((x,y))] = [clue,answerList]
 
 #            FINISH THIS, MUST USE COUNT ADJACENT (CHANGE TO RETURN TEMP ARRAYS)
 #            WILL TAKE ARRAY AND ADD TO WHAT TO PUT IN BLUESCLUES
@@ -108,6 +121,8 @@ class AdvancedAgent:
         #bluesclues is the dictionary of all clauses, with key being coordinate of clue
         # and values in bluesclues is a set of all the unvisited neighbors
         self.bluesclues = {}
+
+        return None
 
 
 
@@ -139,65 +154,3 @@ class AdvancedAgent:
 
 
         return allHidden
-
-
-
-
-        #for every set of coordinates in the hidden neighbors of this clue
-
-
-# CHECK IF ENOUGH ASSIGNS ARE POSSIBLE TO MAKE IT True
-
-
-    # Update KB given clue at given cell at (x,y)
-
-    #COMMENTED FOR NOW, REWRITING
-    #def update_kb(self, x, y, clue):
-        #pass
-
-
-
-
-    #update the kb to reflect a new clause based on the newly revelead square
-    #this new additional clause can create potentially new conclusions
-    #we can seek out contradictions efficiently by finding the least used variables (hidden squares)
-    #to find this, we need to include the commons dictionary, which will include each clue value as a key with
-    # the number of other clues that include this coordinate in their clauses
-
-    '''
-
-    bluesclues
-    allvarslist
-    is_mine
-    is_safe
-    revealed
-
-    def update_kb(self, x, y, clue):
-
-        if (clue == "mine"):
-            # Hit a mine
-            self.is_mine[x][y] = 1
-            self.revealed[x][y] = 1
-            self.n_hidden -= 1
-            for(item in self.bluesclues.items()):
-                if((x,y) in item[1]):
-                    item[0]-=1
-                    item.remove((x,y))
-        else:
-            self.is_safe[x][y] = 1
-            self.revealed[x][y] = 1
-            self.n_hidden -= 1
-            tempSet = {}
-
-            self.bluesclues[(x,y)] = []
-
-    '''
-
-# TESTING STARTS HERE
-
-
-
-        #clues dict: coordinate : number of surrounding mines
-        #clause - coordinate of clue : set of clauses attached
-        #clues array, track what exists
-        #hidden array, track the unknown
